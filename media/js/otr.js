dee3afab391e5a7bf404cce26111627b339006b6
irisgyq <gouyingqiyanxuan321@gmail.com>
2015-06-06 01:20:05 +0800

    submit button

---
+++ media/js/otr.js
@@ -0,0 +1,6 @@
+ $('#start-otr').on('click', function () {
+    window.onload = function(){
+        var a = document.getElementById('lcb-otr-description');
+        div.innerHTML = div.innerHTML.replace(/The chat is encrypted./g,'The chat is not encrypted').replace(/fa-lock/g,'fa-unlock');
+    } 
+  })
