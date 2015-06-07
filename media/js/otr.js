8a7172f2388ed7df9a38bb72ffef39ad9581e34b
irisgyq <gouyingqiyanxuan321@gmail.com>
2015-06-08 00:02:42 +0800

    get a key when submit

--- media/js/otr.js
+++
@@ -1,6 +0,0 @@
- $('#start-otr').on('click', function () {
-    window.onload = function(){
-        var a = document.getElementById('lcb-otr-description');
-        div.innerHTML = div.innerHTML.replace(/The chat is encrypted./g,'The chat is not encrypted').replace(/fa-lock/g,'fa-unlock');
-    } 
-  })
